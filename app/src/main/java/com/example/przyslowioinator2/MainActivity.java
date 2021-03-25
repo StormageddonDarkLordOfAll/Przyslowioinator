58371c9b713076dfcc5d8d1f2c8fa2ebf1b4c045
StormageddonDarkLordOfAll <57874179+StormageddonDarkLordOfAll@users.noreply.github.com>
2021-03-25 20:00:31 +0100

    test1
    test kontaktu z baza

--- app/src/main/java/com/example/przyslowioinator2/MainActivity.java
+++ app/src/main/java/com/example/przyslowioinator2/MainActivity.java
@@ -1,14 +1,117 @@
 package com.example.przyslowioinator2;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import java.util.concurrent.ThreadLocalRandom;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> przyslowia=new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button losujButton;
+        losujButton = (Button) findViewById(R.id.losujButton);
+        losujButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                // goToMainActivity();
+                losujRequest();
+            }
+        });
+
+        przyslowia  = pobierzPrzyslowia();
+
+
+
+
+    }
+    private ArrayList<String>  pobierzPrzyslowia() {
+        //wyslanie do babzy danych zapytanych
+        ArrayList<String> slowa = new ArrayList<String>();
+        try {
+
+            //wrzucenie podanych danych do jsona
+            JSONObject jo = new JSONObject();
+            jo.put("haslo","maslo");
+
+            String url= "http://10.0.2.2:5000/requestBase";
+            JsonObjectRequest sr = new JsonObjectRequest(Request.Method.GET, url, jo, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Boolean isSuccess = false;
+                    String stringError = "";
+
+                    try {
+                        isSuccess = response.getBoolean("success");
+                        stringError = response.getString("errorString");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    //Jesli uda sie pobrac z bazy
+                    if(isSuccess){
+                        Toast.makeText(getApplicationContext(),"Konto utworzone",Toast.LENGTH_LONG).show();
+                        //goToLoginActivity();
+                    }else{
+                        Toast.makeText(getApplicationContext(),stringError,Toast.LENGTH_LONG).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+                }
+            });
+
+            //RequestSingleton.getInstance(this).addToRequestQueue(sr);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return slowa;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void losujRequest(){//ArrayList<String> slowa, String sciezka
+
+            //jesli wszystkie przyslowia zuzyte
+            if(this.przyslowia.size()<1) {
+                przyslowia = pobierzPrzyslowia();
+            }
+            if(przyslowia.size()<1) {
+                //przyslowia.add("ERROR BRAK PRZYSLOW");
+                Toast.makeText(getApplicationContext(),"Error brak przyslow",Toast.LENGTH_LONG).show();
+                return;
+            }
+            int losowy = ThreadLocalRandom.current().nextInt(0, przyslowia.size()); //+1
+            String linia = przyslowia.get(losowy);
+        przyslowia.remove(losowy);
+        ClipboardManager clipboard = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        }
+        ClipData clip = ClipData.newPlainText("Przyslowie", linia);
+        clipboard.setPrimaryClip(clip);
+
     }
 }
